a087c6bff51b1df86b757bfef188db391ca376de
Gianna Nguyen <gploriana@gmail.com>
2024-11-07 16:14:02 -0800

    created studio 2 and added beginning code for the project

---
+++ studio2/script.js
@@ -0,0 +1,14 @@
+(function () {
+
+    "use strict";
+    console.log('running js');
+    
+    const sobaImg = document.querySelector('#sobaImg');
+
+    sobaImg.addEventListener('click', function() {
+        img.className = "border";
+        console.log('running function');
+    });
+
+    
+})();
