79d3c4a54a1cc8acdded1ebbd9a8ac1abcbbde33
Gianna Nguyen <gploriana@gmail.com>
2024-11-14 12:40:49 -0800

    added features for photo incrementation and css for the blend effects, not completed

--- studio2/script.js
+++ studio2/script.js
@@ -3,12 +3,59 @@
     "use strict";
     console.log('running js');
     
-    const sobaImg = document.querySelector('#sobaImg');
+    const enterLeft = [
+        'left1.png',
+        'left2.png',
+        'left3.png'
+    ];
 
-    sobaImg.addEventListener('click', function() {
-        img.className = "border";
-        console.log('running function');
-    });
+    const enterRight = [
+        'right1.png',
+        'right2.png',
+        'right3.png'
+    ];
+
+    const main = document.querySelector('main');
+    const info = document.querySelector('header p');
+
+    let leftImage, rightImage = 0;
+    const leftSlider = document.getElementById("left_slider");    const rightSlider = document.getElementById("right_slider");
 
     
+    document.getElementById('next').addEventListener('click', nextSlide);
+
+    function nextSlide() {
+        console.log('running function');
+        leftImage++; 
+        rightImage++;
+        //increment the counter
+        //set the source for the slide to the next image
+        if (leftImage > enterLeft.length - 1 && rightImage > enterRight.length - 1) {
+            //If the user is at the end of the array...
+            leftImage = 0; 
+            rightImage = 0;
+        }
+        //how can i best write my js so I can have both sides enter at the same time, and also increment when the "next" button is pressed?
+        leftSlider.src = `images/${enterLeft[leftImage]}`;
+        rightSlider.src = `images/${enterRight[rightImage]}`;
+
+    }
+
+    document.getElementById('previous').addEventListener('click', previousSlide);
+
+    function previousSlide() {
+        leftImage--; 
+        rightImage--; 
+        //decrement the counter
+        //set the source for the slide to the next image
+
+        if (leftImage < 0 && rightImage < 0) {
+            //If the user is at the beginning of the array...
+            leftImage = enterLeft.length - 1;
+            rightImage = enterRight.length - 1;
+        }
+        leftSlider.src = `images/${enterLeft[leftImage]}`;
+        rightSlider.src = `images/${enterRight[rightImage]}`;
+    }
+
 })();
